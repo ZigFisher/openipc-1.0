b395b6224cf91580f4ba4086ed4e0b24f8195cf8
Claus Marxmeier <claus@marxmeier.de>
2019-07-24 22:07:25 +0200

    Update build_smartfrog.sh

--- build_smartfrog.sh
+++ build_smartfrog.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-if [ ! -d feeds ]
-then
-   ./scripts/feeds update -a
-   ./scripts/feeds install -a
+if [ ! -d feeds ]; then
+./scripts/feeds update -a
+./scripts/feeds install -a
 fi
 
 ./Project_OpenIPC.sh smartfrog
